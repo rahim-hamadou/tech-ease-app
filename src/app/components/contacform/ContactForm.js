e73eed22ba0c69f4eb5a2173bf4783021c529c16
rumgal <rahimhamadou@gmail.com>
2023-11-18 00:25:42 +0100

    fix .enctype

--- src/app/components/contacform/ContactForm.js
+++ src/app/components/contacform/ContactForm.js
@@ -7,7 +7,7 @@ function ContactForm() {
 			className={styles.form_container}
 			action="mailto:techeaseassociation@gmail.com"
 			method="get"
-			enctype="text/plain"
+			encType="text/plain"
 		>
 			Nom
 			<input type="text" name="subject" placeholder="Votre nom" />
